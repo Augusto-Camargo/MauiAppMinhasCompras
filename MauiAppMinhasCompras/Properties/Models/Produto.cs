eaed29ff8591321ac7f4ac7f520dbf9ee9428a99
Augusto_Camargo <augustodasilvadecamargo14@gmail.com>
2026-08-13 12:44:25 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Properties/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppMinhasCompras.Properties.Models
+{
+    internal class Produto
+    {
+    }
+}
---
+++ MauiAppMinhasCompras/Resources/Produto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MauiAppMinhasCompras.Resources
+{
+    internal class Produto
+    {
+    }
+}
