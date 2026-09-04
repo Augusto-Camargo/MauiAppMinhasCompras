0c44c6710212c0a2db5f136c0bf5f1586e0fa766
Augusto_Camargo <augustodasilvadecamargo14@gmail.com>
2026-09-04 12:26:48 -0300

    Organização dos erros em : Salvar

--- MauiAppMinhasCompras/Properties/Models/Produto.cs
+++ MauiAppMinhasCompras/Properties/Models/Produto.cs
@@ -8,13 +8,50 @@ namespace MauiAppMinhasCompras.Properties.Models
     public class Produto
     {
 
+
+        string _descricao;
+        double _quantidade;
+        double _preco;
+        double _total;
+
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Descrição não pode ser nula");
+                }
+                _descricao = value;
+            }
+        }
 
-        public double Quantidade { get; set; }
+        public double Quantidade { 
+            get => _quantidade;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Quantidade não pode ser zero");
+                }
+                _quantidade = value;
+            }
+        }
 
-        public double Preco {  get; set; }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("Preço não pode ser zero");
+                }
+                _preco = value;
+            }
+        }
 
         public double Total { get => Quantidade * Preco; }
 
--- MauiAppMinhasCompras/Resources/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Resources/Views/ListaProduto.xaml.cs
@@ -19,6 +19,8 @@ public partial class ListaProduto : ContentPage
 
         try{
 
+            Lista.Clear();
+
         List<Produto> tmp = await App.Db.GetAll();
 
         tmp.ForEach(i => Lista.Add(i));
